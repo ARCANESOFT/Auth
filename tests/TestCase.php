650eb2f9398a4c9fd7f8b2cf69fc366c01a86f7f
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-11-13 19:38:17 +0000

    Updating TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -24,7 +24,7 @@ abstract class TestCase extends BaseTestCase
     protected function getPackageProviders($app)
     {
         return [
-            \Arcanesoft\Auth\AuthServiceProvider::class,
+            \Arcanesoft\Foundation\FoundationServiceProvider::class,
         ];
     }
 
