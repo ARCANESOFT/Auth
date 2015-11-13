2652267f599fe4d99b31497f629fbe7c64150c8f
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-11-13 19:46:28 +0000

    Adding verify administrator middleware

---
+++ src/Http/Middleware/VerifyAdministrator.php
@@ -0,0 +1,64 @@
+<?php namespace Arcanesoft\Auth\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+
+/**
+ * Class     VerifyAdministrator
+ *
+ * @package  Arcanesoft\Auth\Http\Middleware
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class VerifyAdministrator
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * The Guard implementation.
+     *
+     * @var Guard
+     */
+    protected $auth;
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Constructor
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Create a new filter instance.
+     *
+     * @param  Guard  $auth
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure                  $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($this->auth->check()) {
+            /** @var \Arcanedev\LaravelAuth\Models\User $user */
+            $user = $this->auth->user();
+
+            if ($user->isAdmin()) {
+                return $next($request);
+            }
+        }
+
+        return response('Unauthorized.', 401);
+    }
+}
