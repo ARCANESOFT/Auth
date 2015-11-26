814467c8594789e4388a1c04e236b078261524fc
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-11-26 17:54:07 +0000

    Adding User model

---
+++ src/Models/User.php
@@ -0,0 +1,14 @@
+<?php namespace Arcanesoft\Auth\Models;
+
+use Arcanedev\LaravelAuth\Models\User as BaseUserModel;
+
+/**
+ * Class     User
+ *
+ * @package  Arcanesoft\Auth\Models
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class User extends BaseUserModel
+{
+    //
+}
