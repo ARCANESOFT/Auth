1d96a49018e049b320e8693331e362137fa8a1a9
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-12-02 20:22:31 +0000

    Adding the profile navigation menu

--- src/Models/User.php
+++ src/Models/User.php
@@ -7,8 +7,25 @@ use Arcanedev\LaravelAuth\Models\User as BaseUserModel;
  *
  * @package  Arcanesoft\Auth\Models
  * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ *
+ * @property  string  gravatar
  */
 class User extends BaseUserModel
 {
-    //
+    /* ------------------------------------------------------------------------------------------------
+     |  Getters & Setters
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Get the gravatar attribute.
+     *
+     * @return string
+     */
+    public function getGravatarAttribute()
+    {
+        return gravatar()
+            ->setDefaultImage('mm')
+            ->setSize(160)
+            ->src($this->email, $this->username);
+    }
 }
