bdb7fcb8cc99df6fa0bb4f2d05a1d66472f5dfd5
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-12-17 01:20:27 +0000

    Updating the config file

--- config/auth.php
+++ config/auth.php
@@ -1,7 +1,48 @@
 <?php
 
 return [
-    'route' => [
-        'prefix'    => 'authorization',
+    'route'             => [
+        'prefix' => 'authorization',
+    ],
+
+    'database'          => [
+        'connection' => config('database.default'),
+    ],
+
+    'users'             => [
+        'table' => 'users',
+        'model' => Arcanesoft\Auth\Models\User::class,
+    ],
+
+    'roles'             => [
+        'table' => 'roles',
+        'model' => Arcanesoft\Auth\Models\Role::class,
+    ],
+
+    'permissions'       => [
+        'table' => 'permissions',
+        'model' => Arcanesoft\Auth\Models\Permission::class,
+    ],
+
+    'user-confirmation' => [
+        'enabled'   => false,
+        'length'    => 30,
+    ],
+
+    'throttles'         => [
+        'enabled' => true,
+        'table'   => 'throttles',
+    ],
+
+    'slug-separator'    => '.',
+
+    'seeds'             => [
+        'users' => [
+            [
+                'username'   => 'admin',
+                'email'      => env('ADMIN_USER_EMAIL', 'admin@email.com'),
+                'password'   => env('ADMIN_USER_PASSWORD', 'password'),
+            ],
+        ],
     ],
 ];
