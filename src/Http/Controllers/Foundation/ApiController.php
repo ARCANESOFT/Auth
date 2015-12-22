f3524c5e78a85eaa899bea584cdfcae18e012d6a
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-12-22 12:57:43 +0000

    Adding API Controllers

---
+++ src/Http/Controllers/Foundation/ApiController.php
@@ -0,0 +1,14 @@
+<?php namespace Arcanesoft\Auth\Http\Controllers\Foundation;
+
+use Arcanesoft\Auth\Bases\Controller;
+
+/**
+ * Class     ApiController
+ *
+ * @package  Arcanesoft\Auth\Http\Controllers\Foundation
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class ApiController extends Controller
+{
+
+}
---
+++ src/Http/Controllers/Front/ApiController.php
@@ -0,0 +1,56 @@
+<?php namespace Arcanesoft\Auth\Http\Controllers\Front;
+
+use Arcanesoft\Auth\Bases\Controller;
+use Illuminate\Contracts\Auth\Guard;
+
+/**
+ * Class     ApiController
+ *
+ * @package  Arcanesoft\Auth\Http\Controllers\Front
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class ApiController extends Controller
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * @var \Illuminate\Contracts\Auth\Guard
+     */
+    private $auth;
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Constructor
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * ApiController constructor.
+     *
+     * @param  \Illuminate\Contracts\Auth\Guard  $auth
+     */
+    public function __construct(Guard $auth)
+    {
+        parent::__construct();
+
+        $this->auth = $auth;
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    public function check()
+    {
+        self::onlyAjax();
+
+        if ( ! $this->auth->check()) {
+            abort(404, 'Page not found');
+        }
+
+        return [
+            'success' => true,
+            'message' => 'User authenticated.',
+        ];
+    }
+}
