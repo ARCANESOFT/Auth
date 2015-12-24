2a6336455b1b05d52c1b73edcde11126e9e40154
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-12-24 14:51:22 +0000

    Adding Model Observers

---
+++ src/Observers/RoleObserver.php
@@ -0,0 +1,14 @@
+<?php namespace Arcanesoft\Auth\Observers;
+
+use Arcanedev\LaravelAuth\Observers\RoleObserver as BaseObserver;
+
+/**
+ * Class     RoleObserver
+ *
+ * @package  Arcanesoft\Auth\Observers
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class RoleObserver extends BaseObserver
+{
+    //
+}
