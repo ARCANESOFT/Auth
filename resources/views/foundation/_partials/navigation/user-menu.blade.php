2ed7bb87ba67405f85de455bc6d803cf72b0ba5d
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-12-29 23:36:19 +0000

    Updating user navigation menus (views)

--- resources/views/foundation/_partials/navigation/user-menu.blade.php
+++ resources/views/foundation/_partials/navigation/user-menu.blade.php
@@ -1,44 +1,42 @@
-@if (Auth::check())
-    <?php
+<?php
     /** @var  \Arcanesoft\Auth\Models\User  $user */
-    $user = Auth::user();
-    ?>
-    <li class="dropdown user user-menu">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-            {!! Html::image($user->gravatar, $user->full_name, ['class' => 'user-image']) !!}
-            <span class="hidden-xs">{{ $user->full_name }}</span>
-        </a>
-        <ul class="dropdown-menu">
-            {{-- User image --}}
-            <li class="user-header">
-                {!! Html::image($user->gravatar, $user->full_name, ['class' => 'img-circle']) !!}
-                <p>{{ $user->full_name }} <small>{{ $user->since_date }}</small></p>
-            </li>
-            {{-- Menu Body --}}
-            <?php /*
-                <li class="user-body">
-                    <div class="row">
-                        <div class="col-xs-4 text-center">
-                            <a href="#">Followers</a>
-                        </div>
-                        <div class="col-xs-4 text-center">
-                            <a href="#">Sales</a>
-                        </div>
-                        <div class="col-xs-4 text-center">
-                            <a href="#">Friends</a>
-                        </div>
+    $user = auth()->user();
+?>
+<li class="dropdown user user-menu">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        {!! Html::image($user->gravatar, $user->full_name, ['class' => 'user-image']) !!}
+        <span class="hidden-xs">{{ $user->full_name }}</span>
+    </a>
+    <ul class="dropdown-menu">
+        {{-- User image --}}
+        <li class="user-header">
+            {!! Html::image($user->gravatar, $user->full_name, ['class' => 'img-circle']) !!}
+            <p>{{ $user->full_name }} <small>{{ $user->since_date }}</small></p>
+        </li>
+        {{-- Menu Body --}}
+        <?php /*
+            <li class="user-body">
+                <div class="row">
+                    <div class="col-xs-4 text-center">
+                        <a href="#">Followers</a>
                     </div>
-                </li>
-             */ ?>
-            {{-- Menu Footer --}}
-            <li class="user-footer">
-                <div class="pull-left">
-                    {!! link_to_route('auth::foundation.profile.index', trans('auth::generals.profile'), [], ['class' => 'btn btn-default btn-flat']) !!}
-                </div>
-                <div class="pull-right">
-                    {!! link_to_route('auth::logout', trans('auth::generals.sign-out'), [], ['class' => 'btn btn-default btn-flat']) !!}
+                    <div class="col-xs-4 text-center">
+                        <a href="#">Sales</a>
+                    </div>
+                    <div class="col-xs-4 text-center">
+                        <a href="#">Friends</a>
+                    </div>
                 </div>
             </li>
-        </ul>
-    </li>
-@endif
+         */ ?>
+        {{-- Menu Footer --}}
+        <li class="user-footer">
+            <div class="pull-left">
+                {!! link_to_route('auth::foundation.profile.index', trans('auth::generals.profile'), [], ['class' => 'btn btn-default btn-flat']) !!}
+            </div>
+            <div class="pull-right">
+                {!! link_to_route('auth::logout', trans('auth::generals.sign-out'), [], ['class' => 'btn btn-default btn-flat']) !!}
+            </div>
+        </li>
+    </ul>
+</li>
--- resources/views/public/_partials/navigation.blade.php
+++ resources/views/public/_partials/navigation.blade.php
@@ -1,7 +1,7 @@
-@if (Auth::check())
+@if (auth()->check())
     <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-            {{ Auth::user()->full_name }} <span class="caret"></span>
+            {{ auth()->user()->full_name }} <span class="caret"></span>
         </a>
         <ul class="dropdown-menu">
             <li>
