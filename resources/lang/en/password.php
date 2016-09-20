e65555b605564fd84cb53e27b2e941fe1361278d
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-09-20 19:02:27 +0100

    Updating translations

---
+++ resources/lang/en/password.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'heading' => 'Reset Password',
+    'submit'  => 'Send Password Reset Link',
+    'links'   => [
+        'login'    => 'You\'ve remembered your password ? Sign in',
+        'register' => 'Don\'t have an account ? Sign up',
+    ],
+    'mail'    => [
+        'line-1'  => 'You are receiving this email because we received a password reset request for your account.',
+        'line-2'  => 'If you did not request a password reset, no further action is required.',
+        'action'  => 'Reset Password',
+    ],
+];
--- resources/lang/fr/login.php
+++ resources/lang/fr/login.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'heading'  => 'Bienvenue!',
-    'remember' => 'Se souvenir de moi',
-    'submit'   => 'Se connecter',
-    'links'    => [
+    'heading'     => 'Se connecter',
+    'remember-me' => 'Se souvenir de moi',
+    'submit'      => 'Se connecter',
+    'links'       => [
         'register' => 'Vous n\'avez pas un compte? Inscrivez-vous',
-        'reset'    => 'Avez-vous oublié votre mot de passe?',
+        'forget'   => 'Mot de passe oublié?',
     ],
 ];
