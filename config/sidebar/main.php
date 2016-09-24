905a05ae0eff82b5a1cff401bfefb55409294e7d
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-09-25 00:29:14 +0100

    Updating the sidebar items

--- config/sidebar/main.php
+++ config/sidebar/main.php
@@ -1,10 +1,12 @@
 <?php
 
+use Arcanesoft\Auth\Models\Role;
+
 return [
     'title'       => 'Authorization',
     'name'        => 'auth',
     'icon'        => 'fa fa-fw fa-key',
-    'roles'       => [],
+    'roles'       => [Role::ADMINISTRATOR],
     'permissions' => [],
     'children'    => [
         [
@@ -12,29 +14,32 @@ return [
             'name'        => 'auth-dashboard',
             'route'       => 'auth::foundation.dashboard',
             'icon'        => 'fa fa-fw fa-bar-chart',
-            'roles'       => [],
+            'roles'       => [Role::ADMINISTRATOR],
             'permissions' => ['auth.dashboard.stats'],
-        ],[
+        ],
+        [
             'title'       => 'Users',
             'name'        => 'auth-users',
             'route'       => 'auth::foundation.users.index',
             'icon'        => 'fa fa-fw fa-users',
-            'roles'       => [],
+            'roles'       => [Role::ADMINISTRATOR],
             'permissions' => ['auth.users.list'],
-        ],[
+        ],
+        [
             'title'       => 'Roles',
             'name'        => 'auth-roles',
             'route'       => 'auth::foundation.roles.index',
             'icon'        => 'fa fa-fw fa-lock',
-            'roles'       => [],
+            'roles'       => [Role::ADMINISTRATOR],
             'permissions' => ['auth.roles.list'],
-        ],[
+        ],
+        [
             'title'       => 'Permissions',
             'name'        => 'auth-permissions',
             'route'       => 'auth::foundation.permissions.index',
             'icon'        => 'fa fa-fw fa-check-circle',
-            'roles'       => [],
+            'roles'       => [Role::ADMINISTRATOR],
             'permissions' => ['auth.permissions.list'],
-        ]
+        ],
     ],
 ];
