44c771551643f3c86f959f9195b823c7be55714c
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-12-02 15:20:18 +0000

    Adding a sharable dashboard

---
+++ resources/views/foundation/_includes/dashboard.blade.php
@@ -0,0 +1,13 @@
+<div class="row">
+    <div class="col-sm-6 col-md-3">
+        @include(Arcanesoft\Auth\ViewComposers\Dashboard\UsersCountComposer::VIEW)
+    </div>
+    <div class="col-sm-6 col-md-3">
+        @include(Arcanesoft\Auth\ViewComposers\Dashboard\RolesCountComposer::VIEW)
+    </div>
+    <div class="col-sm-6 col-md-3">
+        @include(Arcanesoft\Auth\ViewComposers\Dashboard\PermissionsCountComposer::VIEW)
+    </div>
+</div>
+
+@include(\Arcanesoft\Auth\ViewComposers\Dashboard\LatestThirtyDaysCreatedUsersComposer::VIEW)
--- resources/views/foundation/dashboard.blade.php
+++ resources/views/foundation/dashboard.blade.php
@@ -3,19 +3,7 @@
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-sm-6 col-md-3">
-            @include(Arcanesoft\Auth\ViewComposers\Dashboard\UsersCountComposer::VIEW)
-        </div>
-        <div class="col-sm-6 col-md-3">
-            @include(Arcanesoft\Auth\ViewComposers\Dashboard\RolesCountComposer::VIEW)
-        </div>
-        <div class="col-sm-6 col-md-3">
-            @include(Arcanesoft\Auth\ViewComposers\Dashboard\PermissionsCountComposer::VIEW)
-        </div>
-    </div>
-
-    @include(\Arcanesoft\Auth\ViewComposers\Dashboard\LatestThirtyDaysCreatedUsersComposer::VIEW)
+    @include('auth::foundation._includes.dashboard')
 @endsection
 
 @section('scripts')
