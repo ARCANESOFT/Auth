477928a289faa775cb832180cf9161f95a3dba1e
ARCANEDEV <arcanedev.maroc@gmail.com>
2017-01-29 13:31:57 +0000

    Cleaning the views

--- resources/views/public/_includes/impersonation-nav-item.blade.php
+++
@@ -1,6 +0,0 @@
-@if (Arcanedev\LaravelAuth\Services\UserImpersonator::isEnabled() && Arcanedev\LaravelAuth\Services\UserImpersonator::isImpersonating())
-    <li>
-        {{ link_to_route('auth::users.impersonate.stop', 'Stop Impersonate') }}
-    </li>
-    <li class="divider"></li>
-@endif
