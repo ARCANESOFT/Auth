e0d24d1173580a4ae08186fe429feda7b6192028
ARCANEDEV <arcanedev.maroc@gmail.com>
2017-04-08 19:37:11 +0100

    Cleaning the translations

--- resources/lang/en/register.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'heading' => 'Sign up',
-    'submit'  => 'Sign up',
-    'links'   => [
-        'login' => 'Already have an account ? Sign in',
-    ],
-];
--- resources/lang/fr/register.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'heading' => 'S\'inscrire',
-    'submit'  => 'S\'inscrire',
-    'links'   => [
-        'login' => 'Avez-vous déjà un compte ? Connectez-vous',
-    ],
-];
