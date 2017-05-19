6f0d059d1906e2cd9c71741baeb5a3d9709305ec
ARCANEDEV <arcanedev.maroc@gmail.com>
2017-05-19 13:53:55 +0100

    Adding permission check for the dashboard

--- resources/views/admin/_composers/dashboard.blade.php
+++ resources/views/admin/_composers/dashboard.blade.php
@@ -1,16 +1,18 @@
-<div class="row">
-    <div class="col-sm-6 col-md-3">
-        @include(Arcanesoft\Auth\ViewComposers\Dashboard\UsersCountComposer::VIEW)
+@can(Arcanesoft\Auth\Policies\DashboardPolicy::PERMISSION_STATS)
+    <div class="row">
+        <div class="col-sm-6 col-md-3">
+            @include(Arcanesoft\Auth\ViewComposers\Dashboard\UsersCountComposer::VIEW)
+        </div>
+        <div class="col-sm-6 col-md-3">
+            @include(Arcanesoft\Auth\ViewComposers\Dashboard\RolesCountComposer::VIEW)
+        </div>
+        <div class="col-sm-6 col-md-3">
+            @include(Arcanesoft\Auth\ViewComposers\Dashboard\PermissionsCountComposer::VIEW)
+        </div>
+        <div class="col-sm-6 col-md-3">
+            @include(Arcanesoft\Auth\ViewComposers\Dashboard\OnlineUsersCountComposer::VIEW)
+        </div>
     </div>
-    <div class="col-sm-6 col-md-3">
-        @include(Arcanesoft\Auth\ViewComposers\Dashboard\RolesCountComposer::VIEW)
-    </div>
-    <div class="col-sm-6 col-md-3">
-        @include(Arcanesoft\Auth\ViewComposers\Dashboard\PermissionsCountComposer::VIEW)
-    </div>
-    <div class="col-sm-6 col-md-3">
-        @include(Arcanesoft\Auth\ViewComposers\Dashboard\OnlineUsersCountComposer::VIEW)
-    </div>
-</div>
 
-@include(Arcanesoft\Auth\ViewComposers\Dashboard\LatestThirtyDaysCreatedUsersComposer::VIEW)
+    @include(Arcanesoft\Auth\ViewComposers\Dashboard\LatestThirtyDaysCreatedUsersComposer::VIEW)
+@endcan
